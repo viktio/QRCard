c9f155af059b827a1174b7f14cf7b8a569fe9dcc
viktio <wyrzykowskimm@gmail.com>
2014-11-02 22:33:05 +0100

    Aggresive progress

---
+++ DB/qrc.sql
@@ -0,0 +1,21 @@
+use qrc;
+
+create table users (
+username varchar(50),
+password varchar(50),
+created datetime,
+	last_login datetime
+	);
+
+create table 
+QRDATA
+(
+username varchar(50),
+name varchar(200),
+surname varchar(200),
+phone_no varchar(50),
+mail varchar(100),
+picture_name varchar(50),
+qrcode varchar(200)
+);
+
